35e514d34c53ed6a9c274c02a769f774a9ce70bb
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-11-24 20:20:51 +0900

    Feat : added a failing testcode

---
+++ src/test/java/com/example/demo/service/SimpleServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleServiceTest {
+    private final SimpleService simpleService = new SimpleService();
+
+    @Test
+    public void setEvenNumberTest(){
+        simpleService.setEvenNumber(3);
+    }
+}
