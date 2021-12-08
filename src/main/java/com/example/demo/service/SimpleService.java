43b968adbf4e978ae6619efee4b8c7980f1741a1
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-08 20:38:21 +0900

    Test : google formatter test

--- src/main/java/com/example/demo/service/SimpleService.java
+++ src/main/java/com/example/demo/service/SimpleService.java
@@ -1,10 +1,47 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Random;
+
 public class SimpleService {
 
     public void setEvenNumber(int number) {
         if (number % 2 != 0) {
             throw new IllegalArgumentException("[ERROR] 짝수가 아닙니다");
         }
+        for (int i = 0; i < 5; i++) {
+
+        }
+        for (int i = 0; i < 5; i++) {
+
+        }
+        for (int i = 0; i < 5; i++) {
+
+        }
+        for (int i = 0; i < 5; i++) {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
     }
 }
