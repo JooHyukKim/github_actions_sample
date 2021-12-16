ae1b592746c08881f9e059e4f1e820b02064ee79
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-16 21:37:49 +0900

    Feat : cache build

--- src/integrationTest/java/com/example/demo/IntegrationTest.java
+++ src/integrationTest/java/com/example/demo/IntegrationTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.logging.Logger;
+
 @SpringBootTest
 public class IntegrationTest {
 
     @Test
     void testIntegrations() {
         Assertions.assertEquals(2, 2);
+        System.out.println("------------------------Integration");
     }
 }
