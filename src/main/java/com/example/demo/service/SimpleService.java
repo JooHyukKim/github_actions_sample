b6e8ce008d0f085bb1417dbb0c2530724cbff9e4
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-28 22:20:36 +0900

    Feat : test sonar

--- src/main/java/com/example/demo/service/SimpleService.java
+++ src/main/java/com/example/demo/service/SimpleService.java
@@ -2,16 +2,28 @@ package com.example.demo.service;
 
 public class SimpleService {
 
-    public void setEvenNumber(int number) {
-        if (number % 2 != 0) {
-            throw new IllegalArgumentException("[ERROR] 짝수가 아닙니다");
-        }
-        for (int i = 0; i < 5; i++) {}
-
-        for (int i = 0; i < 5; i++) {}
-
-        for (int i = 0; i < 5; i++) {}
-
-        for (int i = 0; i < 5; i++) {}
+  public void setEvenNumber(int number) {
+    if (number % 2 != 0) {
+      throw new IllegalArgumentException("[ERROR] 짝수가 아닙니다");
     }
+    for (int i = 0; i < 5; i++) {
+    }
+
+    for (int i = 0; i < 5; i++) {
+    }
+
+    for (int i = 0; i < 5; i++) {
+    }
+
+    for (int i = 0; i < 5; i++) {
+    }
+  }
+
+  public int untestedMethod() {
+    return Integer.MAX_VALUE;
+  }
+
+  public int untestedMethod1() {
+    return Integer.MIN_VALUE;
+  }
 }
